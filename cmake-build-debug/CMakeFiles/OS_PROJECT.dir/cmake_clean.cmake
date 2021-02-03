f72ff3245974e1c30336939fbc5f0db09fc69a06
Seiya <sasmith10@student.ysu.edu>
2021-02-03 02:34:35 -0500

    vdiFunctions added somewhat

---
+++ cmake-build-debug/CMakeFiles/OS_PROJECT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_PROJECT.dir/main.cpp.o"
+  "OS_PROJECT"
+  "OS_PROJECT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_PROJECT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
