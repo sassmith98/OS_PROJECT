950fe5fe084045b39f6b16e36a88828d29f62ea7
Seiya <sasmith10@student.ysu.edu>
2021-05-01 01:37:12 -0400

    Addition of parition functions, superblock functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "testVDI.h"
+
+using namespace std;
+int open(char *fn, int mode);
+void close(int fd);
+ssize_t read(int fd, void *buf, size_t count);
+ssize_t write(int fd, void *buf, size_t count);
+off_t lseek(int fd, off_t offset, int anchor);
+void displayBufferPage(uint8_t *buf, uint32_t count, uint32_t skip, uint64_t offset);
+void displayBuffer(uint8_t *buf, uint32_t count, uint64_t offset);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    displayBuffer(headerBuffer, 400, 0);
+
     return 0;
 }
