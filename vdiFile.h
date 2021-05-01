d6fbad9e60e9ef961d1053bcf1f7140d81384961
Seiya <sasmith10@student.ysu.edu>
2021-05-01 02:21:30 -0400

    data files added

--- vdiFile.h
+++ vdiFile.h
@@ -1,8 +1,18 @@
 //
 // Created by csis on 3/20/21.
 //
-
 #ifndef OS_PROJECT_VDIFILE_H
 #define OS_PROJECT_VDIFILE_H
+#include "vdiHeader.h"
+#include <fstream>
+#pragma pack(1)
+
+struct vdiFile{
+    int pageSize;
+    int fileDescriptor;
+    int pointerPosition;
+    vdiHeader header;
+};
+
 
 #endif //OS_PROJECT_VDIFILE_H
